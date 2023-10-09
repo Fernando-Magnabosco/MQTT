b8bca2cae8e10e00ecaf0279b65773a8afc309bf
Fernando-Magnabosco <fernandomagnabosco1@gmail.com>
2023-10-09 15:42:39 -0300

    .

--- index2.js
+++ index2.js
@@ -1,35 +1,62 @@
-location.port = 5002;
-
-// Create a client instance
-var client = new Paho.MQTT.Client(location.hostname, Number(location.port), "clientId");
-
-// set callback handlers
-client.onConnectionLost = onConnectionLost;
-client.onMessageArrived = onMessageArrived;
-
-// connect the client
-client.connect({onSuccess:onConnect});
+// // location.port = 5002;
+// console.log(location);
+// // Create a client instance
+// var client = new Paho.MQTT.Client(location.hostname, 1884, "clientId");
+// client.debug = true;
+// // set callback handlers
+// client.onConnectionLost = onConnectionLost;
+// client.onMessageArrived = onMessageArrived;
 
 
-// called when the client connects
-function onConnect() {
-    return
-  // Once a connection has been made, make a subscription and send a message.
-  console.log("onConnect");
-  client.subscribe("World");
-  message = new Paho.MQTT.Message("Hello");
-  message.destinationName = "World";
-  client.send(message);
+
+// // connect the client
+// client.connect({onSuccess: onConnect, mqttVersion: 3});
+
+
+// // called when the client connects
+// function onConnect () { 
+//   // Once a connection has been made, make a subscription and send a message.
+//   console.log("onConnect");
+//   client.subscribe("World");
+//     const message = new Paho.MQTT.Message("Hello");
+//   message.destinationName = "World";
+//   client.send(message);
+// }
+
+// // called when the client loses its connection
+// function onConnectionLost(responseObject) {
+//   if (responseObject.errorCode !== 0) {
+//     console.log("onConnectionLost:"+responseObject.errorMessage);
+//   }
+// }
+
+// // called when a message arrives
+// function onMessageArrived(message) {
+//   console.log("onMessageArrived:"+message.payloadString);
+// }
+
+
+// Initialize MQTT client
+var client = mqtt.connect('ws://localhost:1884');
+
+// Set up event handlers for the client
+client.on('connect', function () {
+    console.log('Connected to MQTT broker');
+    // Subscribe to a topic when connected
+    client.subscribe('World');
+    client.publish('World', 'Hello mqtt');
+});
+
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log(message.toString());        
+});
+
+// on key press send message
+document.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        client.publish('World', 'Hello mqtt');
+        console.log("Pedro Manfio Lill");
+    }
 }
-
-// called when the client loses its connection
-function onConnectionLost(responseObject) {
-  if (responseObject.errorCode !== 0) {
-    console.log("onConnectionLost:"+responseObject.errorMessage);
-  }
-}
-
-// called when a message arrives
-function onMessageArrived(message) {
-  console.log("onMessageArrived:"+message.payloadString);
-}
+);
