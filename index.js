1700da8794b83a76fcf8a3a3db8e9f2be5be0263
Fernando-Magnabosco <fernandomagnabosco1@gmail.com>
2023-12-03 13:02:30 -0300

    btn close bs class

--- index.js
+++ index.js
@@ -95,8 +95,7 @@ function User () {
                     <div class="modal-content">
                         <div class="modal-header">
                             <h5 class="modal-title" id="exampleModalLabel">Chat com ${userId}</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-                                <span aria-hidden="true">&times;</span>
+                            <button type="button" class="btn btn-close" data-dismiss="modal" aria-label="Fechar">
                             </button>
                         </div>
                         <div class="modal-body">
