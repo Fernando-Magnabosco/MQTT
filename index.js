26ed3a02fb7db2161aa3812e51a58de84fc53faf
Pedro Manfio Lill <pedro.manfiolill@gmail.com>
2023-12-03 05:01:40 -0300

    ajustes parciais

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import constants from './constants.js';
+
 var C = new constants();
 
 function User () {
@@ -87,14 +88,67 @@ function User () {
 
     this.onChatClick = function (event) {
         const userId = $(event.target).data('user-id');
-
-        const inputBox = $(`<div class="input-group mb-3 mt-1">
-        <input type="text" class="form-control" placeholder="Digite sua mensagem" aria-label="Recipient's username" aria-describedby="button-addon2">
-        <button class="btn btn-outline-secondary" type="button" id="button-addon2">Enviar</button>
-        </div>`);
-        inputBox.on('click', '#button-addon2', this.onSendMessage.bind(this));
-        this.ui.userList.find(`#${userId}`).append(inputBox);
+    
+        // Criar o modal
+        const inputBox = `
+            <div class="modal fade" id="chatModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Chat com ${userId}</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
+                                <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <ul id="chatMessages" class="list-group"></ul>
+                            <div class="input-group mb-3 flex-nowrap">
+                                <input type="text" class="form-control" id="messageInput" placeholder="Digite sua mensagem" aria-label="Recipient's username" aria-describedby="button-addon2" style="background-color: transparent; color: white;">
+                            </div>
+                        </div>
+                        <div class="modal-footer">
+                            <button class="btn btn-outline-secondary" type="button" id="button-addon2">Enviar</button>
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    
+        $(document.body).append(inputBox);
+    
+        const me = this;
+    
+        $('#chatModal #messageInput').on('keyup', function (event) {
+            if (event.keyCode === 13) {
+                $('#chatModal #button-addon2').click();
+            }
+        });
+    
+        $('#chatModal').on('shown.bs.modal', function () {
+            $('#chatModal #messageInput').focus();
+        });
+    
+        $('#chatModal #button-addon2').on('click', function () {
+            const message = $('#chatModal #messageInput').val();
+            if (message.trim() !== '') {
+                const topic = me.chatUsers[userId];
+                const messageObj = {sender: me.id, content: message, timestamp: new Date().getTime()};
+                me.client.publish(topic, JSON.stringify(messageObj));
+                $('#chatModal #messageInput').val('');
+    
+                console.log('Mensagem enviada:', message);
+            }
+    
+            $('#chatModal').modal('hide');
+        });
+    
+        $('#chatModal').modal('show');
     }
+    
+    
+    
+    
 
     this.onSendMessage = function (event) {
         const userId = $(event.target).parent().parent().attr('id');
@@ -144,25 +198,39 @@ function User () {
     }
 
     this.seeChat = function (event) {
-        const topic = this.chatUsers[$(event.target).data('user-id')];
-        const chat = this.messageHistory[topic];
-        const chatBox = $(`<div class="chat-box"></div>`);
-        if (!chat) return;
-        chat.forEach(message => {
-            chatBox.append(`
-            <div class="card mt-1">
-                <div class="card-body">
-                    <h5 class="card-title">${message.sender}</h5>
-                    <p class="card-text">${message.content}</p>
+    const userId = this.id; // Obtém o ID do usuário atual
+    const topic = this.chatUsers[$(event.target).data('user-id')];
+    const chat = this.messageHistory[topic];
+    const chatBox = $(`<div class="chat-box"></div>`);
+
+    if (!chat) return;
+
+    chat.forEach(message => {
+        // Adiciona uma classe para distinguir as mensagens do remetente atual
+        const isCurrentUser = message.sender === userId;
+        const messageClass = isCurrentUser ? 'current-user-message' : 'other-user-message';
+
+        // Adiciona a mensagem ao chatBox com a classe apropriada
+        chatBox.append(`
+            <div class="card mt-1 chat-message ${messageClass}">
+                <div class="useravatar">
+                    <span class="rounded-circle d-flex justify-content-center align-items-center bg-dark text-white" style="width: 35px; height: 35px; font-size: 1.2rem; margin-bottom: 5px;">
+                        <i class="fas fa-user"></i> ${userId.charAt(0).toUpperCase() + userId.charAt(1).toUpperCase()}
+                    </span>
                 </div>
-                <div class="card-footer text-muted">
-                    ${message.timestamp}
+                <div class="message-bubble">
+                    <h5 class="card-text">${message.content}</h5>
+                </div>
+                <div class="card-footer text-muted" style="color: #000000 !important;">
+                    <p class="card-time">${message.timestamp}</p>
                 </div>
             </div>`);
-        });
+    });
 
-        this.ui.chatBox.html(chatBox);
-    }
+    this.ui.chatBox.html(chatBox);
+};
+
+    
 
     this.onUsersMessage = function (message) {
         const [userId, status] = message.toString().split(' ');
@@ -172,20 +240,22 @@ function User () {
             this.client.publish(C.USERS, this.connectMessage);
             this.knownUsers[userId] = status;
             this.ui.userList.append(
-                `<li id="${userId}" role="button" class="list-group-item">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <span>${userId}</span>
-                            <span class="badge ${status === 'online' ? 'bg-primary' : 'bg-danger'} badge-pill">${status}</span>
-                        </div>
-                        <div>
-                            <button data-user-id="${userId}" type="button" class="btn btn-primary btn-sm float-right user-accept d-none">Aceitar solicitacao</button>
-                            <button data-user-id="${userId}" type="button" class="btn btn-primary btn-sm float-right start-a-chat">Comecar novo chat</button>
-                            <button data-user-id="${userId}" type="button" class="btn btn-primary btn-sm float-right user-chat d-none">Chat</button>
-                            <button data-user-id="${userId}" type="button" class="btn btn-primary btn-sm float-right see-chat d-none">Ver conversas</button>
-                        </div>
+                `<li id="${userId}" role="button" class="list-group-item mb-3">
+                <div class="d-flex justify-content-between">
+                    <div class="d-flex flex-column">
+                        <span class="rounded-circle d-flex justify-content-center align-items-center bg-dark text-white" style="width: 35px; height: 35px; font-size: 1.2rem; margin-bottom: 5px;">
+                            <i class="fas fa-user"></i> ${userId.charAt(0).toUpperCase() + userId.charAt(1).toUpperCase()}
+                        </span>
+                        <span class="badge ${status === 'online' ? 'badge badge-success' : 'badge-danger'} badge-pill bg-success">${status}</span>
                     </div>
-                </li>`);
+                    <div>
+                        <button data-user-id="${userId}" type="button" class="btn btn-primary btn-sm float-right user-accept d-none" style="background-color:#FF5733">Aceitar solicitacao</button>
+                        <button data-user-id="${userId}" type="button" class="btn btn-primary btn-sm float-right start-a-chat" style="background-color:#FF5733">Iniciar chat</button>
+                        <button data-user-id="${userId}" type="button" class="btn btn-primary btn-sm float-right user-chat d-none" style="background-color:#FF5733" >Chat</button>
+                        <button data-user-id="${userId}" type="button" class="btn btn-primary btn-sm float-right see-chat d-none" style="background-color:#FF5733">Ver conversas</button>
+                    </div>
+                </div>
+            </li>`);
         }
 
         if (status === C.OFFLINE) {
@@ -265,7 +335,7 @@ function User () {
                 `<div class="serMembro btn btn-primary" data-group-name="${grupo.nome}" data-dono="${grupo.dono}"type="button" class="btn btn-primary btn-sm float-right">Ser membro</div>`
 
                 : ''}
-                    <button class="verMembros btn btn-primary" data-group-name="${grupo.nome}" type="button" class="btn btn-primary btn-sm float-right">Ver grupo</button>    
+                    <button class="verMembros btn btn-primary" data-group-name="${grupo.nome}" type="button" class="btn btn-primary btn-sm float-right  ">Ver grupo</button>    
                 </div>
             </li>`);
 
@@ -278,7 +348,7 @@ function User () {
     }
 
     this.onVerMembrosClick = function (event) {
-        const nomeDoGrupo = $(event.target).data('group-name');
+        const nomeDoGrupo = $(event.taarget).data('group-name');
         console.log(nomeDoGrupo, this.groups[nomeDoGrupo]);
     }
 
